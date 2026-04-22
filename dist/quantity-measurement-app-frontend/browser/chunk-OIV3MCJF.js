35e87643f57fbfe26d0a88e2d9ebea1f97ffd1be
Shivam Sharma <shivam832005sharma@gmail.com>
2026-04-22 13:24:14 +0530

    [Shivam Sharma].Refactor - refactored the code for deployment

---
+++ dist/quantity-measurement-app-frontend/browser/chunk-OIV3MCJF.js
@@ -0,0 +1,1 @@
+var t={production:!0,apiUrl:"https://quantitymeasurementapp-9.onrender.com"};export{t as a};
--- dist/quantity-measurement-app-frontend/browser/chunk-YWH2XBMR.js
+++
@@ -1,1 +0,0 @@
-var e={production:!1,apiUrl:"https://quantitymeasurementapp-9.onrender.com"};export{e as a};
